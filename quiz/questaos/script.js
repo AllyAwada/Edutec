24c6413d427b907503071bb736e5ec9f55584897
Ally Awada <Allyhawada13@gmail.com>
2023-06-08 04:07:21 -0300

    first commit

---
+++ quiz/questaos/script.js
@@ -0,0 +1,26 @@
+function gh1() {
+    document.querySelector(".qt1").innerHTML = `<a class="qts" href="#q1">1</a>`
+}
+function gh2() {
+    document.querySelector(".qt2").innerHTML = `<a class="qts" href="#q2">2</a>`
+}
+function gh3() {
+    document.querySelector(".qt3").innerHTML = `<a class="qts" href="#q3">3</a>`
+}
+function gh4() {
+    document.querySelector(".qt4").innerHTML = `<a class="qts" href="#q4">4</a>`
+}
+function gh5() {
+    document.querySelector(".qt5").innerHTML = `<a class="qts" href="#q5">5</a>`
+}
+function gh6() {
+    document.querySelector(".qt6").innerHTML = `<a class="qts" href="#q6">6</a>`
+}
+function gh7() {
+    document.querySelector(".qt7").innerHTML = `<a class="qts" href="#q7">7</a>`
+}
+
+
+function fn() {
+    document.querySelector(".f1").innerHTML = `<a class="fn1" href="/">Finalizar</a>`
+}
